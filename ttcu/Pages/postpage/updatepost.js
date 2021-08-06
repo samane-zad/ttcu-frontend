dcbfeff63be0247cf991c82ededfaa352c29d48b
samane-zad <zadabbassamane@gmail.com>
2021-08-06 15:10:47 +0430

    Modified delete script

---
+++ ttcu/Pages/postpage/updatepost.js
@@ -0,0 +1,29 @@
+$(function () {
+$("input").prop("disabled", true);
+    $("textarea").prop("disabled", true);
+
+    $(".edit").click(function () {
+      $("html").addClass("active");
+    $("input").prop("disabled", false);
+      $("textarea").prop("disabled", false);
+    });
+    $(".save").click(function () {
+      $("html").removeClass("active");
+    $("input").prop("disabled", true);
+      $("textarea").prop("disabled", true);
+    });
+    function resizeInput() {
+      $(this).attr("size", $(this).val().length + 2);
+    }
+  $("input").keyup(resizeInput).each(resizeInput);
+
+
+    function resizeTextArea() {
+        $(this).attr("size", $(this).val().length + 8);
+      }
+    
+    $("textarea").keyup(resizeTextArea).each(resizeTextArea);
+
+  });
+  
+  
