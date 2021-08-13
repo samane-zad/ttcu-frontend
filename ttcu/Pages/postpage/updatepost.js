2a1e506244a495ef69ecf6260b3645dcd481d83f
samane-zad <zadabbassamane@gmail.com>
2021-08-13 22:46:13 +0430

    Added postInquiry and updatePost script

--- ttcu/Pages/postpage/updatepost.js
+++ ttcu/Pages/postpage/updatepost.js
@@ -1,29 +1,34 @@
 $(function () {
-$("input").prop("disabled", true);
-    $("textarea").prop("disabled", true);
-
-    $(".edit").click(function () {
-      $("html").addClass("active");
-    $("input").prop("disabled", false);
+  $("input").prop("disabled", true);
+  $("textarea").prop("disabled", true);
+  $("select").prop("disabled", true);
+  
+      $(".edit").click(function () {
+        $("html").addClass("active");
+      $("input").prop("disabled", false);
       $("textarea").prop("disabled", false);
+      $("select").prop("disabled", false);
     });
-    $(".save").click(function () {
-      $("html").removeClass("active");
-    $("input").prop("disabled", true);
+      $(".save").click(function () {
+        $("html").removeClass("active");
+      $("input").prop("disabled", true);
       $("textarea").prop("disabled", true);
+      $("select").prop("disabled", true);
     });
-    function resizeInput() {
-      $(this).attr("size", $(this).val().length + 2);
-    }
-  $("input").keyup(resizeInput).each(resizeInput);
-
-
-    function resizeTextArea() {
-        $(this).attr("size", $(this).val().length + 8);
+  
+      function resizeInput() {
+        $(this).attr("size", $(this).val().length + 2);
       }
+    $("input").keyup(resizeInput).each(resizeInput);
+  
     
-    $("textarea").keyup(resizeTextArea).each(resizeTextArea);
-
-  });
   
+      function resizeTextArea() {
+          $(this).attr("size", $(this).val().length + 8);
+        }
+      
+      $("textarea").keyup(resizeTextArea).each(resizeTextArea);
   
+    });
+    
+    
