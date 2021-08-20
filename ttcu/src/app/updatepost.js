2e1bf587a0a82f41ba393a2c507d2826f4ed61cb
samane-zad <zadabbassamane@gmail.com>
2021-08-20 16:00:39 +0430

    -

--- ttcu/src/app/updatepost.js
+++ ttcu/src/app/updatepost.js
@@ -1,32 +1,4 @@
 $(function () {
-<<<<<<< HEAD:ttcu/src/app/updatepost.js
-$("input").prop("disabled", true);
-$("textarea").prop("disabled", true);
-$("select").prop("disabled", true);
-
-    $(".edit").click(function () {
-      $("html").addClass("active");
-    $("input").prop("disabled", false);
-    $("textarea").prop("disabled", false);
-    $("select").prop("disabled", false);
-  });
-    $(".save").click(function () {
-      $("html").removeClass("active");
-    $("input").prop("disabled", true);
-    $("textarea").prop("disabled", true);
-    $("select").prop("disabled", true);
-  });
-
-    function resizeInput() {
-      $(this).attr("size", $(this).val().length + 2);
-    }
-  $("input").keyup(resizeInput).each(resizeInput);
-
-  
-
-    function resizeTextArea() {
-        $(this).attr("size", $(this).val().length + 8);
-=======
   $("input").prop("disabled", true);
   $("textarea").prop("disabled", true);
   $("select").prop("disabled", true);
@@ -46,7 +18,6 @@ $("select").prop("disabled", true);
   
       function resizeInput() {
         $(this).attr("size", $(this).val().length + 2);
->>>>>>> 2a1e506244a495ef69ecf6260b3645dcd481d83f:ttcu/Pages/postpage/updatepost.js
       }
     $("input").keyup(resizeInput).each(resizeInput);
   
